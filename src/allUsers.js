a1e58b10936b89c4ea69274929adfa58f51a3534
Shamsur Raza Chowdhury <shamsur314@gmail.com>
2022-06-25 18:43:36 +0300

    all users can be retrieved

---
+++ src/allUsers.js
@@ -0,0 +1,18 @@
+const AWS = require('aws-sdk');
+
+const allUsers = async (event) => {
+    const dynamodb = new AWS.DynamoDB.DocumentClient();
+    const params = {
+        TableName: 'UserTable'
+    };
+    const {Items} = await dynamodb.scan(params).promise();
+    const allEmails = Items.map((item) => item.email);
+    return {
+        statusCode: 200,
+        body: JSON.stringify(allEmails)
+    };
+}
+
+module.exports = {
+    handler: allUsers
+}
